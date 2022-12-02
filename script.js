66af7886500f74861a7e0d92df95d06687b72914
Baptiste BLANC <baptisteblanc.pro@gmail.com>
2022-12-02 05:06:59 +0100

    commit

--- script.js
+++ script.js
@@ -5,3 +5,46 @@ $(document).ready(function(){
         $('ul').toggleClass('show');
     });
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i = 0;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    // console.log(typeof(slides))
+
+    // console.log(slides.length)
+    // console.log(n)
+
+    // for (i = 0; i < 6; i++) {
+    //     slides[i].style.display = 1 ;  
+    // }
+
+    if (n > 6) {slideIndex = 1}  
+    if (n < 1) {slideIndex = 6}
+    
+    // console.log(slideIndex)
+    // console.log(slides.length)
+
+    for (i = 0; i < 6; i++) {
+        slides[i].style.display = "none";  
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
